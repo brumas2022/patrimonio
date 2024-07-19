6c6250782223d7df4fe866e99d52165f228a86bf
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-19 15:23:25 -0400

    Update licitacao.py

--- licitacao.py
+++ licitacao.py
@@ -2,6 +2,20 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config("Licitações de materiais", layout="wide")
-st.sidebar.button("INSERIR")
-st.sidebar.button("CONSULTAR")
-st.write("Aqui vou relacionar as licitações para iniciar o processo")
+
+def inserir():
+    st.write("Este é o modulo de inserção")
+
+def consulta():
+    st.write("Este é o modulo de consulta")
+  
+i=st.sidebar.button("INSERIR")
+c=st.sidebar.button("CONSULTAR")
+
+if i:
+   inserir
+
+if c:
+   consulta
+
+
