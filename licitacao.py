19a677cfb7b3a03ce9c64a7e1f27e367a072541c
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-19 15:19:26 -0400

    Update licitacao.py

--- licitacao.py
+++ licitacao.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-st_set_page("Licitações de materiais", layout="wide")
+st.set_page_config("Licitações de materiais", layout="wide")
 st.write("Aqui vou relacionar as licitações para iniciar o processo")
