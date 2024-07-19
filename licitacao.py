67659ea355c6312c8c8b7de3abb815e93387511d
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-19 15:33:05 -0400

    Update licitacao.py

--- licitacao.py
+++ licitacao.py
@@ -1,11 +1,32 @@
 import streamlit as st
 import pandas as pd
+import psycopg2
 
 st.set_page_config("Licitações de materiais", layout="wide")
 
-def inserir():
+def inserir(obj, dataini):
     st.write("Este é o modulo de inserção")
+    try:
+     connection = psycopg2.connect(
+               host='aws-0-sa-east-1.pooler.supabase.com',
+               user='postgres.ibhcxtnwnonsnycfgjay',
+               password='Hoje#estamos#firmes#como#geleia',
+               database='postgres',
+               port='5432'
 
+     )
+     st.write("conexao exitosa")
+     cursor = connection.cursor()
+     comando = f"""INSERT INTO licitacao (objeto, datainicial) VALUES ('{obj}', '{dataini}')"""
+     cursor.execute(comando)
+     connection.commit()
+     st.text("Cadastro efetuado com sucesso")
+     cursor.close()
+     connection.close()
+         
+    except Exception as ex:
+            st.write(ex)
+        
 def consulta():
     st.write("Este é o modulo de consulta")
   
@@ -13,6 +34,7 @@ i=st.sidebar.button("INSERIR")
 c=st.sidebar.button("CONSULTAR")
 
 if i:
+    
    inserir()
 
 if c:
