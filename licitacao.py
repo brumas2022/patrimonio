a2ebc87852150a0dbb85f47beb1b5618267844c7
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-19 15:57:43 -0400

    Update licitacao.py

--- licitacao.py
+++ licitacao.py
@@ -35,10 +35,12 @@ c=st.sidebar.button("CONSULTAR")
 
 if i:
     with st.form("ENTRA", clear_on_submit=True):
-        col = st.columns((1,1))
-        a1 = col[0].text_input("Objeto da licitacao")
-        a2 = col[0].text_input("Data inicial")
-        inserir(a1,a2)
+         col = st.columns((1,1))
+         a1 = col[0].text_input("Objeto da licitacao")
+         a2 = col[0].text_input("Data inicial")
+         enviar = st.form_submit_button("ENTRA")
+         if enviar:
+            inserir(a1,a2)
   
 
 if c:
