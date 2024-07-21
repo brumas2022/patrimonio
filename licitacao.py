0d2d97f0e3e739004cc9f6ee02da37f1f28a40fe
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-21 12:11:14 -0300

    Update licitacao.py

--- licitacao.py
+++ licitacao.py
@@ -6,26 +6,33 @@ st.set_page_config("Licitações de materiais", layout="wide")
 
 def inserir(a1, a2):
     st.write("Este é o modulo de inserção")
-    try:
-     connection = psycopg2.connect(
-               host='aws-0-sa-east-1.pooler.supabase.com',
-               user='postgres.hdhvkseneldllvnlvpgc',
-               password='Hoje#estamos#fortes#como#geleia',
-               database='postgres',
-               port='5432'
+    with st.form("ENTRA", clear_on_submit=True):
+         col = st.columns((1,1))
+         a1 = col[0].text_input("Objeto da licitacao")
+         a2 = col[0].text_input("Data inicial")
+         enviar = st.form_submit_button("ENTRA")
+         if enviar:
+    
+            try:
+               connection = psycopg2.connect(
+                         host='aws-0-sa-east-1.pooler.supabase.com',
+                         user='postgres.hdhvkseneldllvnlvpgc',
+                         password='Hoje#estamos#fortes#como#geleia',
+                         database='postgres',
+                         port='5432'
 
-     )
-     st.write("conexao exitosa")
-     cursor = connection.cursor()
-     comando = f"""INSERT INTO Patrimonio (objeto, datainicial) VALUES ('{a1}', '{a2}')"""
-     cursor.execute(comando)
-     connection.commit()
-     st.text("Cadastro efetuado com sucesso")
-     cursor.close()
-     connection.close()
+               )
+               st.write("conexao exitosa")
+               cursor = connection.cursor()
+               comando = f"""INSERT INTO Patrimonio (objeto, datainicial) VALUES ('{a1}', '{a2}')"""
+               cursor.execute(comando)
+               connection.commit()
+               st.text("Cadastro efetuado com sucesso")
+               cursor.close()
+               connection.close()
          
-    except Exception as ex:
-            st.write(ex)
+           except Exception as ex:
+                  st.write(ex)
         
 def consulta():
     st.write("Este é o modulo de consulta")
