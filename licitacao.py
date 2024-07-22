f8d09c474c66a30ceb3c08fbbfd8d351c64bf953
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-22 16:38:35 -0400

    Update licitacao.py

--- licitacao.py
+++ licitacao.py
@@ -58,6 +58,7 @@ def consulta():
                resultado1.columns=['Id', 'Entrada', 'Licitacao', 'Data'] 
                resultado1.set_index("Id", inplace=True) 
                resultado1['Data'] = pd.to_datetime(resultado1.Data)
+               resultado1['Data'] = resultado1['Data'].dt.strftime('%m/%d/%Y')
                st.dataframe(resultado1)
          
     except Exception as ex:
