879e1adbc755189a2ae446ce373e1a8e1de7c990
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-22 15:47:51 -0400

    Update licitacao.py

--- licitacao.py
+++ licitacao.py
@@ -54,7 +54,7 @@ def consulta():
                comando = "SELECT * FROM Patrimonio"
                cursor.execute(comando)
                resultado=cursor.fetchall()
-               resultado.header=["Id", "Entrada", "Licitacao", "Data"] 
+               resultado.columns=['Id', 'Entrada', 'Licitacao', 'Data'] 
                st.dataframe(resultado)
          
     except Exception as ex:
