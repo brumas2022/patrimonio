a61b09f4e237cc0e5bc86931c3f495ef5e1a2120
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-23 11:13:48 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd 
+
+st.set_page_config("Consulta estoque", layout="wide")
+
+consulta = st.selectbox("Escolha o tipo de consulta", ("POR ITEM", "POR NOME")
+
+                        if consulta=="POR ITEM":
+                           st.write("Consulta por item")
+                        elif consulta=="POR NOME":
+                           st.write("Consulta por nome")
 
