4494ea6b624d673f2639dabcfe98786d5b6fd093
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-23 16:47:58 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -11,8 +11,7 @@ if consulta=="POR ITEM":
    st.write("Consulta por ordem numerica")
    df.columns=['Item', 'Descrição', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
    df.set_index("Item", inplace=True)
-   df1 = pd.read_clipboard()
-   st.dataframe(df1)
+  
    st.dataframe(df.iloc[3:])
    
    #selecao = df['Item']=="1974"
