dc07bb4383029f6607391adc6c63f8fbc614fa4a
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-23 15:02:55 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -11,8 +11,8 @@ if consulta=="POR ITEM":
    st.write("Consulta por ordem numerica")
    df.columns=['Item', 'Descrição', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
    df.set_index("Item", inplace=True)
-   df.head(6)
-   st.dataframe(df)
+   
+   st.dataframe(df.head(3))
 elif consulta=="POR NOME":
    st.write("Consulta por ordem alfabetica")
 
