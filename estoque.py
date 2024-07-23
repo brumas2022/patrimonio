d088c0d3b1c19b8c133be17b7164922779463ae1
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-23 11:20:09 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -5,7 +5,7 @@ st.set_page_config("Consulta estoque", layout="wide")
 
 df=pd.read_excel("RPosicao_Estoque_Data_Atual_Excel.xlsx", sheet_name=1)
 
-consulta = st.selectbox("Escolha o tipo de consulta", ("POR ITEM", "POR NOME")
+consulta = st.selectbox("Escolha o tipo de consulta", ("POR ITEM", "POR NOME"))
 
                         if consulta=="POR ITEM":
                            st.write("Consulta por item")
