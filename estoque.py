0f312f03a425cbd8863cb871aa439c63d09d1fa0
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-23 11:21:10 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -7,10 +7,10 @@ df=pd.read_excel("RPosicao_Estoque_Data_Atual_Excel.xlsx", sheet_name=1)
 
 consulta = st.selectbox("Escolha o tipo de consulta", ("POR ITEM", "POR NOME"))
 
-                        if consulta=="POR ITEM":
-                           st.write("Consulta por item")
-                           st.dataframe(df)
-                        elif consulta=="POR NOME":
-                           st.write("Consulta por nome")
+if consulta=="POR ITEM":
+   st.write("Consulta por item")
+   st.dataframe(df)
+elif consulta=="POR NOME":
+   st.write("Consulta por nome")
 
 
