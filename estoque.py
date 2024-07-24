19e231648dc39f92c066318209f03fd9a2f55936
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-24 15:55:58 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -25,7 +25,7 @@ elif consulta=="POR NOME":
    a = st.selectbox("Escolha a descrição :", nomes, index=None, placeholder="Digite o nome....")
 
    resultado = df[df['Descricao']==a]
-   st.dataframe(resultado.loc[1])
+   st.dataframe(resultado)
    
 elif consulta=="TODOS":
    st.dataframe(df.iloc[3:])
