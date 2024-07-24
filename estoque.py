6339e6eb599a1cc334777097e195a6f1f40c0f35
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-24 16:58:07 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -15,7 +15,8 @@ if consulta=="POR ITEM":
    item = df['Item'].tolist()
    b = st.selectbox("Escolha o item", item, index=None, placeholder="Digite o nro...")
    resultado_item = df[df['Item']==b]
-   st.dataframe(resultado_item.iat[1,1])
+   st.dataframe(resultado_item)
+   st.write(resultado_item.iat[1,1])
    
    #df.set_index("Item", inplace=True)
          
