b60ec3db47139c7482e6c43baf20db576b8c3aec
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-24 11:13:35 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -5,25 +5,19 @@ import openpyxl
 st.set_page_config("Consulta estoque", layout="wide")
 
 df=pd.read_excel("RPosicao_Estoque_Data_Atual_Excel.xlsx", sheet_name=0)
+df.columns=['Item', 'Descricao', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
 
 consulta = st.selectbox("Escolha o tipo de consulta", ("POR ITEM", "POR NOME"))
 
 if consulta=="POR ITEM":
    st.write("Consulta por ordem numerica")
-   df.columns=['Item', 'Descricao', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
    #df.set_index("Item", inplace=True)
-  
    st.dataframe(df.iloc[3:])
    valor = 19
  
    resultado = df[df['Item'] == valor]
    st.dataframe(df.iloc[valor])
-
-   nomes = df['Descricao'].tolist()
-   a = st.selectbox("Escolha a descrição :", nomes)
-
-   resultado = df[df['Descricao']==a]
-   st.dataframe(resultado)
+   
    
    #selecao = df['Item']=="1974"
    #line = df.loc[df["Item"] == '1974']
@@ -31,5 +25,10 @@ if consulta=="POR ITEM":
 
 elif consulta=="POR NOME":
    st.write("Consulta por ordem alfabetica")
+   nomes = df['Descricao'].tolist()
+   a = st.selectbox("Escolha a descrição :", nomes)
+
+   resultado = df[df['Descricao']==a]
+   st.dataframe(resultado)
 
 
