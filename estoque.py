a72893dd0d21059e05d326b9eb0979b1bca7ce28
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-29 10:57:13 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -5,7 +5,7 @@ import openpyxl
 st.set_page_config("Consulta estoque", layout="wide")
 st.image("logosanear.png")
 
-df=pd.read_excel("RPosicao_Estoque_Data_Atual_Excel.xlsx", sheet_name=2)
+df=pd.read_excel("RPosicao_Estoque_Data_Atual_Excel.xlsx", sheet_name=3)
 df.columns=['Item', 'Descricao', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
 
 consulta = st.selectbox("Escolha o tipo de consulta", ("POR ITEM", "POR NOME", "TODOS"))
