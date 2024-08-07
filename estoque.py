4d5de3fa7ece7f2feb1920df06c0ed9630d2336a
brumas2022 <marcos.brumatti@gmail.com>
2024-08-07 16:36:00 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -99,7 +99,7 @@ elif consulta=="ORÇAMENTO":
       pdf.add_page()
       pdf.set_font("Arial")
       pdf.text(115, 145, "Orçamento")
-      pdf.output('orcamento.pdf', 'D')
+      pdf.output("orcamento.pdf")
       #a = webbrowser.open('www.gmail.com', new=1)
       #st.write(a)
       #st.write("Vou testar aqui o desktop")
