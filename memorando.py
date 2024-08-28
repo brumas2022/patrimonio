c5dfab3b72a00f769ffdc0d95d9da750f7109e2b
brumas2022 <marcos.brumatti@gmail.com>
2024-08-28 15:40:38 -0400

    Update memorando.py

--- memorando.py
+++ memorando.py
@@ -53,7 +53,7 @@ def orcamento():
    
    item_orc1=col[0].selectbox("Numero do produto", nomes_orc, index=None, placeholder="Digite o nro....")
    resultado_item = df[df['Item']==item_orc1]
-   teste=resultado_item.item
+   teste=resultado_item[2]
    
    
    col[1].write(teste)
