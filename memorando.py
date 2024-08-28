bca919c3f6e67d3d0c29f5ca163597137d5b254e
brumas2022 <marcos.brumatti@gmail.com>
2024-08-28 15:31:57 -0400

    Update memorando.py

--- memorando.py
+++ memorando.py
@@ -73,7 +73,7 @@ def orcamento():
    qtde6=col[1].text_input("Qtde produto 6 : ")
    
    # CRIAR AS LISTAS 
-   lista_orc.append(str(resultado_item['Descricao'].tolist))
+   lista_orc.append(resultado_item['Descricao'].tolist)
    lista_orc.append(item_orc2)
    lista_orc.append(item_orc3)
    lista_orc.append(item_orc4)
