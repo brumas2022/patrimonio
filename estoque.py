0b33e2fddc010b6aa6635270b6737a4c2606a5c8
brumas2022 <marcos.brumatti@gmail.com>
2024-09-16 09:46:23 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -7,7 +7,7 @@ import webbrowser
 
 
 st.set_page_config("Consulta estoque SANEAR", layout="wide")
-st.image("logosanear.png")
+st.image("logosanear.png", width=100)
 
 def controle():
    a=st.sidebar.button("Agua")
