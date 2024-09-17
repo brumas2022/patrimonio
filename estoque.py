eaddeeb5e28deddddb857726c6c6db14587e7f49
brumas2022 <marcos.brumatti@gmail.com>
2024-09-17 16:02:31 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -63,8 +63,9 @@ def nad():
 
 df=pd.read_excel("RPosicao_Estoque_Data_Atual_Excel.xlsx", sheet_name=39)
 data_atualizacao = df.columns[1]
-st.write("Este aplicativo foi atualizado em :", data_atualizacao)
-#df1=pd.read_excel("RPosicao_Estoque_Data_Atual_Excel.xlsx", sheet_name=39)  ##para testes
+
+st.write("Atualizado em :", data_atualizacao)
+
 df.columns=['Item', 'Descricao', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
 lista_consulta=["POR ITEM", "POR NOME", "TODOS", "ESTOQUE-ZERO", "NAD"]
 
