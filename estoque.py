1c441980f5987a58ecf645e8d5c18c6409270cf6
brumas2022 <marcos.brumatti@gmail.com>
2024-09-17 15:31:18 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -65,10 +65,12 @@ df1=pd.read_excel("RPosicao_Estoque_Data_Atual_Excel.xlsx", sheet_name=39)  ##pa
 df.columns=['Item', 'Descricao', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
 lista_consulta=["POR ITEM", "POR NOME", "TODOS", "ESTOQUE-ZERO", "NAD"]
 
+data_atualizacao = df.columns[1]
+
 consulta = st.selectbox("Escolha o tipo de consulta", (lista_consulta))
 
 if consulta=="POR ITEM":
-   st.write("Consulta por ordem numerica")
+   st.write("Consulta por ordem numerica", data_atualizacao)
    st.info("Nesta seção você precisa saber o numero do item a ser pesquisado")
    item = df['Item'].tolist()
    b = st.selectbox("Escolha o item", item, index=None, placeholder="Digite o nro...")
@@ -91,6 +93,7 @@ elif consulta=="POR NOME":
    a = st.selectbox("Escolha a descrição :", nomes, index=None, placeholder="Digite o nome....")
 
    resultado = df[df['Descricao']==a]
+   
    st.dataframe(resultado, hide_index=True)
    st.write("Data e horario da atualização : ", df1.columns[1])
    
@@ -98,52 +101,6 @@ elif consulta=="TODOS":
    st.write("Data e horario da atualização : ", df1.columns[1])
    st.dataframe(df.iloc[3:], hide_index=True)
 
-elif consulta=="ORÇAMENTO":
-   st.write("Escolha os produtos")
-   lista_orc=[]
-   qtdes=[]
-   nomes_orc = df['Descricao'].tolist()
-   col=st.columns([1,1,1])
-  
-   # INSERIR OS PRODUTOS DO ESTOQUE PARA AQUISICAO
-   
-   item_orc1=col[0].selectbox("Descrição 1", nomes_orc, index=None, placeholder="Digite o nome....")
-   qtde1=col[1].text_input("Qtde produto 1 : ")
-   
-   item_orc2=col[0].selectbox("Descrição 2 :", nomes_orc, index=None, placeholder="Digite o nome....")
-   qtde2=col[1].text_input("Qtde produto 2 : ")
-   
-   item_orc3=col[0].selectbox("Descrição 3 :", nomes_orc, index=None, placeholder="Digite o nome....")
-   qtde3=col[1].text_input("Qtde produto 3 : ")
-
-   # CRIAR AS LISTAS 
-   lista_orc.append(item_orc1)
-   lista_orc.append(item_orc2)
-   lista_orc.append(item_orc3)
-   qtdes.append(qtde1)
-   qtdes.append(qtde2)
-   qtdes.append(qtde3)
-
-   # MOSTRAR NA TELA OS ITENS ESCOLHIDOS
-   lista_total=list(zip(lista_orc, qtdes))
-   df_orc=pd.DataFrame(lista_total, columns=['Descricao', 'Qtde'])
-                       
-   col[0].dataframe(df_orc, use_container_width=True)
-   #df_orc.to_excel("orcamento.xlsx", index=False)
-   #col[1].dataframe(qtdes)
-   df_orc.to_excel("orcamento.xlsx", sheet_name='Planilha1')
-
-   
-
-   # DISPARAR PARA FORNECEDORES
-   enviar = col[0].button("ENVIAR POR EMAIL PARA FORNECEDORES", on_click=imprimir)
-   #enviar = col[0].button("ENVIAR POR EMAIL PARA FORNECEDORES")
-   #col[0].link_button("ENVIAR ORCAMENTO", "https://mail.google.com/mail/u/0/#inbox?compose=new")
-   #col[0].link_button("ENVIAR ORCAMENTO", "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSKkHdPzwgWkcrNGPSHrrKdTGvlGShJwtqqrtkpppgzfSbRtqKjbKXrwzFCvbNDLrMzCrWhP")
-   #from openpyxl.workbook import Workbook
-   #writer = pd.ExcelWriter("/mount/src/patrimonio/amostra.xlsx")
-   #df_orc.to_excel(writer, sheet_name='Planilha1')
-   #writer._save()
    
 elif consulta=="ESTOQUE-ZERO":
    estoque_zero()
