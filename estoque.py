bbb77d51f3f12112622d15291d9abb84a5eab07c
brumas2022 <marcos.brumatti@gmail.com>
2024-09-20 16:27:44 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -25,11 +25,11 @@ def nad():
    st.header("Controle das NADS")
    st.info("Acompanhe o andamento das NADS aqui")
    df_nad=pd.read_excel("controle_nad.xlsx", sheet_name=0)
-   df_new=df_nad.style.format(precision=0, thousands=".", decimal=",").highlight_between(subset='entrega total', left="ok", color="red")
+   df_new=df_nad.style.format(precision=0, thousands=".", decimal=",").highlight_between(subset='entrega total', left="ok", color="red").format(subset=[1], na_rep="MISS")
    
    #st.dataframe(df_nad.style.set_properties(**{'color':'blue', 'background-color':'yellow'}), hide_index=True)
    
-   st.dataframe(df_new, hide_index=True, column_config="data envio")
+   st.dataframe(df_new, hide_index=True)
    #dfteste=pd.read_excel("controle_nad.xlsx", sheet_name=0)
    #dfnovo=dfteste.style.apply(lambda _: "background-color: green", subset=(dfteste.index[1],))
    #st.dataframe(dfnovo)
