fd3211dbc1bfcfc99fbcec634806a670771b3a44
brumas2022 <marcos.brumatti@gmail.com>
2024-09-20 16:20:52 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -29,7 +29,7 @@ def nad():
    
    #st.dataframe(df_nad.style.set_properties(**{'color':'blue', 'background-color':'yellow'}), hide_index=True)
    
-   st.dataframe(df_new, hide_index=True, column_config="data envio", format="%d%m%Y")
+   st.dataframe(df_new, hide_index=True, column_config="data envio")
    #dfteste=pd.read_excel("controle_nad.xlsx", sheet_name=0)
    #dfnovo=dfteste.style.apply(lambda _: "background-color: green", subset=(dfteste.index[1],))
    #st.dataframe(dfnovo)
