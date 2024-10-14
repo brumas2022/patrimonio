150ade9071daac27e1e0f849e0bf517bab2fe18e
brumas2022 <marcos.brumatti@gmail.com>
2024-10-14 16:58:43 -0400

    Update teste_flet.py

--- teste_flet.py
+++ teste_flet.py
@@ -1,11 +1,17 @@
 import flet as ft
 
 def main(pagina):
-    titulo = ft.Text("Hashzap")
+    titulo = ft.Text("Historico escolar")
     pagina.add(titulo)
     
-    botao = ft.ElevatedButton("Iniciar char")
+    
+    botao = ft.ElevatedButton("Primeiro Grau")
+    botao1 = ft.ElevatedButton("Segundo Grau")
+    botao3 = ft.ElevatedButton("Terceiro Grau")
+    
     pagina.add(botao)
+    pagina.add(botao1)
+    pagina.add(botao3)
 
 
 ft.app(main)
