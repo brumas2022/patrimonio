0a5eefc0e0f0031b773ef8d99a202bf13887d6d2
brumas2022 <marcos.brumatti@gmail.com>
2024-11-04 15:52:36 -0400

    Update memorando.py

--- memorando.py
+++ memorando.py
@@ -63,7 +63,7 @@ def orcamento():
    df.columns=['Item', 'Descricao', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
    nomes_orc = df['Item'].tolist()
    
-   print(nomes_orc)
+   #print(nomes_orc)
    
    
    col=st.columns([0.25,0.5,0.25])
@@ -72,10 +72,10 @@ def orcamento():
    global item_orc1, item_orc2, item_orc3, item_orc4, item_orc5, item_orc6
    global qtde1, qtde2, qtde3, qtde4, qtde5, qtde6
    global descricao1, descricao2, descricao3, descricao4, descricao5, descricao6
-   
+   exemplo = ['hoje', 'amanha', 'depois de amanha']
       
    with col[0]:
-       item_orc1=col[0].selectbox("Numero do produto", nomes_orc, index=None)
+       item_orc1=col[0].selectbox("Numero do produto", exemplo, index=None)
        resultado_item1 = df[df['Item']==item_orc1]
        print(resultado_item1)
        descricao1=resultado_item1.iat[0,1]
