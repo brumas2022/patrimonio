8710a8fc1de4f8f8d4509ddd4670742d6d23902e
brumas2022 <marcos.brumatti@gmail.com>
2024-12-04 08:45:36 -0400

    Update calendario.py

--- calendario.py
+++ calendario.py
@@ -1,7 +1,39 @@
 import streamlit as st 
 from streamlit_calendar import calendar
 
-calen = calendar(events={2,3}, options={})
-calen.event("Evento1")
-
-st.write(calen)
+calendar_events = [
+    {
+        "title": "Event 1",
+        "start": "2023-07-31T08:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 2",
+        "start": "2023-07-31T07:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 3",
+        "start": "2023-07-31T10:40:00",
+        "end": "2023-07-31T12:30:00",
+        "resourceId": "a",
+    }
+]
+custom_css="""
+    .fc-event-past {
+        opacity: 0.8;
+    }
+    .fc-event-time {
+        font-style: italic;
+    }
+    .fc-event-title {
+        font-weight: 700;
+    }
+    .fc-toolbar-title {
+        font-size: 2rem;
+    }
+"""
+calen = calendar(events=calendar_events, custom_css=custom_css)
+st.dataframe(calen)
