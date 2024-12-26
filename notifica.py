403b45fe173ecbcda5c2550a18d21367b5a3240e
brumas2022 <marcos.brumatti@gmail.com>
2024-12-26 08:13:54 -0400

    Update notifica.py

--- notifica.py
+++ notifica.py
@@ -1,3 +1,5 @@
 from winotify import Notification
-notificacao = Notification(app_id="Teste", title="VAmos ver se funciona")
+notificacao = Notification(app_id="Bom dia", title="Esta notificao é para ir para o diario oficial")
+notificacao.add_actions(label="aprenda mais", launch="http://www.rondonopolis.mt.gov.br/diario-oficial/")
+
 notificacao.show()
