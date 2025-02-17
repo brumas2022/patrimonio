9b20f25e58867d02a1be7ab5e7078c26dc3da38b
brumas2022 <marcos.brumatti@gmail.com>
2025-02-17 17:02:49 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -17,7 +17,7 @@ def medicao():
    st.info("Acompanhe o andamento da entrega dos materiais deste pregão")
    
    df_medicao = pd.read_excel("PREGAO 13.xlsx", sheet_name=0)
-   df_medicao.columns=["LOTE", "EMPRESA", "VALOR", "DATA EMVIO NAD", "SITUACAO", "PRAZO DE ENTREGA", "DIAS", "OBSERVACAO"]
+   df_medicao.columns=["LOTE", "EMPRESA", "VALOR", "DATA NAD1", "VALOR NAD1", "SITUAÇAO", "DATA NAD2", "VALOR NAD2"]
    
    
    st.dataframe(df_medicao.iloc[3:16], hide_index=True)
