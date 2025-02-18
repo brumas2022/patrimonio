432d73db16f58db56fb58b9a4b72164f201b1b24
brumas2022 <marcos.brumatti@gmail.com>
2025-02-18 16:10:16 -0400

    Update teste_tk.py

--- teste_tk.py
+++ teste_tk.py
@@ -7,8 +7,8 @@ janela.title("Cadastro de audiencias")
 janela.geometry("1000x1000+100+100")
 rotulo = tk.Label(janela, text="Numero do processo")
 entrada = tk.Entry(janela)
-rotulo.pack()
-entrada.pack()
+rotulo.pack(side="left")
+entrada.pack(side="left")
 
 
 
