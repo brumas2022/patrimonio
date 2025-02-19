c778b12d71ee6107692e73367a92b80132733c78
brumas2022 <marcos.brumatti@gmail.com>
2025-02-19 08:26:47 -0400

    Update teste_tk.py

--- teste_tk.py
+++ teste_tk.py
@@ -5,11 +5,14 @@ janela = tk.Tk()
 
 janela.title("Cadastro de audiencias")
 janela.geometry("1000x1000+100+100")
-rotulo = tk.Label(janela, text="Numero do processo")
+nro_processo = tk.Label(janela, text="Numero do processo")
 entrada = tk.Entry(janela)
-rotulo.pack(side="top")
-entrada.pack(side="top")
-
+nro_processo.grid(row=200, column=100)
+entrada.grid(row=200, column=120)
+cliente = tk.Label(janela, text="Nome do cliente")
+nome_cliente = tk.Entry(janela)
+cliente.grid(row=210, column=100)
+nome_cliente.grid(row=210, column=120)
 
 
 janela.mainloop()
