ae133b978e885bc216b19f44c377282a993d5c71
brumas2022 <marcos.brumatti@gmail.com>
2025-02-19 16:58:11 -0400

    Update teste_tk.py

--- teste_tk.py
+++ teste_tk.py
@@ -13,7 +13,13 @@ cliente = tk.Label(janela, text="Nome do cliente")
 nome_cliente = tk.Entry(janela)
 cliente.grid(row=210, column=100)
 nome_cliente.grid(row=210, column=120)
+uc = tk.Label(janela, text="Unidade consumidora do cliente")
+uc_cliente = tk.Entry(janela)
+uc.grid(row=220, column=100)
+uc_cliente.grid(row=220, column=120)
 
+frame = tk.LabelFrame(janela, text="Dados do cliente")
+frame.place(height=100, width=400, rely=0.1, relx=0)
 
 janela.mainloop()
 
