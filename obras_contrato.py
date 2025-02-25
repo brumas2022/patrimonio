7643893268859dfd8b5ed89a64913dd4fc9cf25e
brumas2022 <marcos.brumatti@gmail.com>
2025-02-25 14:34:37 -0400

    Update obras_contrato.py

--- obras_contrato.py
+++ obras_contrato.py
@@ -1,4 +1,7 @@
 import streamlit as st 
+import pandas as pd
+
+df_contratos = pd.read_excel("DADOS_CONTRATOS.xlsx")
 
 st.set_page_config("Consulta contratos de obra", layout="wide")
 lista_contratos=["TECNOBOMBAS", "MASTER", "SPARTACUS", "MENEGUETI"]
@@ -9,8 +12,16 @@ with tab1:
     t11, t12, t13 = st.tabs(lista_dados)
     with t11:
         st.write("Contrado Nro")
+        n=1
+        nro_contrato = col[0].text_input("CONTRATO/ANO", value=f"{df_contratos.iloc[n,1]}")
+        empresa = col[0].text_input("EMPRESA", value=f"{df_contratos.iloc[n,2]}")
+        objeto = col[0].text_input("OBJETO", value=f"{df_contratos.iloc[n,3]}")
+        valor = col[0].text_input("VALOR", value=f"{df_contratos.iloc[n,7]}")
+        data_inicio = col[0].text_input("DATA INICIAL", value=f"{df_contratos.iloc[n,4].strftime("%d/%m/%Y")}")
+        data_fim = col[0].text_input("DATA FINAL", value=f"{df_contratos.iloc[n,5].strftime("%d/%m/%Y")}")
     with t12:
         st.write("Medição nro")
+        
     with t13:
         st.write("Relatorio")
         
