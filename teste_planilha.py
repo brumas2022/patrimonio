5010b2b82686b8561b3292046e395ae30e2afca9
brumas2022 <marcos.brumatti@gmail.com>
2025-03-08 22:57:44 -0300

    Update teste_planilha.py

--- teste_planilha.py
+++ teste_planilha.py
@@ -1,15 +1,27 @@
 import streamlit as st 
 from openpyxl import load_workbook
+import pandas as pd 
 
-wb = load_workbook("teste_outro.xlsx", read_only=False)
+df = pd.read_excel("teste_outro.xlsx")
+tab1, tab2 = st.tabs(["entrada", "planilha"])
 
-ws = wb.active
+with tab1:
+    wb = load_workbook("teste_outro.xlsx", read_only=False)
 
-nome = st.text_input("Qual o seu nome? ")
-dia_nasc = st.text_input("Qual o dia do seu nascimento? ")
-idade = st.text_input("Qautnos anos vc tem?")
+    ws = wb.active
 
-a=st.button("Confirme")
-if a:
-    ws.append([nome, dia_nasc, idade])
-    wb.save("nova.xlsx")
+    nome = st.text_input("Qual o seu nome? ")
+    dia_nasc = st.text_input("Qual o dia do seu nascimento? ")
+    idade = st.text_input("Qautnos anos vc tem?")
+
+    a=st.button("Confirme")
+    if a:
+        ws.append([nome, dia_nasc, idade])
+        wb.save("teste_outro.xlsx")
+        st.rerun()
+        
+with tab2:
+    
+    st.dataframe(df)
+    
+
