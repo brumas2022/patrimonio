a2017e4bc7acfc83dc629340c6671e9d9a110528
brumas2022 <marcos.brumatti@gmail.com>
2025-03-08 22:03:03 -0300

    Update teste_planilha.py

--- teste_planilha.py
+++ teste_planilha.py
@@ -5,5 +5,11 @@ wb = load_workbook("teste_outro.xlsx", read_only=False)
 
 ws = wb.active
 
-ws.append(["mara", "06", "55"])
-wb.save("nova.xlsx")
+nome = st.text_input("Qual o seu nome? ")
+dia_nasc = st.text_input("Qual o dia do seu nascimento? ")
+idade = st.text_input("Qautnos anos vc tem?")
+
+a=st.button("Confirme")
+if a:
+    ws.append([nome, dia_nasc, idade])
+    wb.save("nova.xlsx")
