db9aae62c6926a405362853436e7acd814a28fe1
brumas2022 <marcos.brumatti@gmail.com>
2025-03-12 01:08:43 -0300

    Update teste_excel.py

--- teste_excel.py
+++ teste_excel.py
@@ -1,11 +1,18 @@
 import pandas as pd
-import openpyxl
+#import openpyxl
+from openpyxl import load_workbook
 
-origem = pd.read_excel("orcamento.xlsx", sheet_name=0)
+#origem = pd.read_excel("orcamento.xlsx", sheet_name=0)
 
-print(origem)
+#print(origem)
 
-with pd.ExcelWriter('teste_outro.xlsx', mode='a') as writer:  
-    origem.to_excel(writer, sheet_name='Sheet_name_3')
+#with pd.ExcelWriter('teste_outro.xlsx', mode='a') as writer:  
+#    origem.to_excel(writer, sheet_name='Sheet_name_3')
     
 
+w1 = load_workbook("orcamento.xlsx")
+w2 = load_workbook("teste_outro.xlsx")
+
+ws_to_move = w1['Planilha1']
+ws_to_move._parent = w2
+w2._add_sheet(ws_to_move)
