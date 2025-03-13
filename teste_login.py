960d8e4d359ffa5211601effc91c743973844dcb
brumas2022 <marcos.brumatti@gmail.com>
2025-03-13 00:20:20 -0300

    Update teste_login.py

--- teste_login.py
+++ teste_login.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-if not st.experimental_user.is_logged_in:
+if not st.experimental_user.__dict__:
     if st.button("Log in"):
-        st.login()
+        st.login
 else:
     if st.button("Log out"):
         st.logout()
-    st.write(f"Hello, {st.experimental_user.name}!")
+    st.write(f"Hello, {st.experimental_user.keys}!")
