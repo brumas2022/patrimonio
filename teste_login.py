195e8c1cc190834883fa1389ebda14d4c7537a66
brumas2022 <marcos.brumatti@gmail.com>
2025-03-14 23:25:51 -0300

    Update teste_login.py

--- teste_login.py
+++ teste_login.py
@@ -1,9 +1,25 @@
-import streamlit as st
+import yaml
+import streamlit as st 
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-if not st.experimental_user.__dict__:
-    if st.button("Log in"):
-        st.login
-else:
-    if st.button("Log out"):
-        st.logout()
-    st.write(f"Hello, {st.experimental_user.keys}!")
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+    )
+
+authenticator.login(location='sidebar')
+    
+if st.session_state["authentication_status"]:
+    authenticator.logout(button_name="SAIR", location="sidebar")
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] == None:
+    st.warning('Please enter your username and password')
+    
