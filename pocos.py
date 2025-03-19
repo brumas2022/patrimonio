de2db22653c06bc4802fc2b2282416f9952a095d
brumas2022 <marcos.brumatti@gmail.com>
2025-03-19 15:41:13 -0400

    Update pocos.py

--- pocos.py
+++ pocos.py
@@ -2,18 +2,26 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
+
    
-def p01():
-    st.subheader("P01 - Atlantico", divider="orange")
-    m = folium.Map(location=(-16.478585175815436, -54.64800486531498))
-    st_folium(m)
-    st.button("PAre")
-    #poi = pd.DataFrame({
-    #'lat': [-16.478585175815436],
-    #'lon': [-54.64800486531498],
-    #'name': ['IPANEMA']
-    #})
-    #st.map(poi)
+def p03():
+    st.subheader("P03 - Atlantico", divider="orange")
+    #m = folium.Map(location=(-16.468104952605543, -54.57546160244493))
+    #st_folium(m)
+    #st.button("PAre")
+    poi = pd.DataFrame({
+    'lat': [-16.468104952605543],
+    'lon': [-54.57546160244493],
+    'name': ['ATLANTICO']
+    })
+    st.map(poi)
 def p02():
     st.header("P02 - Cohab", divider="orange")
     st.markdown("Esta é a localização do poço")
+    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+    folium.Marker(
+        [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+    ).add_to(m)
+
+    # call to render Folium map in Streamlit
+    st_data = st_folium(m, width=725)
