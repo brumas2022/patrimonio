c8f24f36ac8eb4a629999da9dc5883f7760b2f14
brumas2022 <marcos.brumatti@gmail.com>
2025-03-18 23:30:09 -0300

    Update elevatorias.py

--- elevatorias.py
+++ elevatorias.py
@@ -1,8 +1,11 @@
 import streamlit as st 
+import folium
+import streamlit_folium 
 def ipanema():
     st.empty()
     st.subheader("EEE IPANEMA", divider="orange")
-    st.image("img.png")
+    m = folium.Map(location=(-16.478585175815436, -54.64800486531498))
+    streamlit_folium.st_folium(m)
 def p01():
     st.subheader("P01 - Atlantico", divider="orange")
     st.markdown("Esta é a localização do poço")
