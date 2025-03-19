185eb3da62914cb1abc8436e3add7d6a8906a312
brumas2022 <marcos.brumatti@gmail.com>
2025-03-19 00:57:33 -0300

    Update elevatorias.py

--- elevatorias.py
+++ elevatorias.py
@@ -1,11 +1,18 @@
 import streamlit as st 
 import folium
 from streamlit_folium import st_folium
+import pandas as pd
 def ipanema():
-    st.empty()
     st.subheader("EEE IPANEMA", divider="orange")
-    m = folium.Map(location=(-16.478585175815436, -54.64800486531498), zoom_control=True)
-    a=st_folium(m)
+    #m = folium.Map(location=(-16.478585175815436, -54.64800486531498))
+    #st_folium(m)
+    #st.button("PAre")
+    poi = pd.DataFrame({
+    'lat': [-16.478585175815436],
+    'lon': [-54.64800486531498],
+    'name': ['IPANEMA']
+    })
+    st.map(poi)
     
 def p01():
     st.subheader("P01 - Atlantico", divider="orange")
