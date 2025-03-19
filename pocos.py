58003e1342c73e92a4c6498490563f1d9a2215c8
brumas2022 <marcos.brumatti@gmail.com>
2025-03-19 17:30:35 -0400

    Update pocos.py

--- pocos.py
+++ pocos.py
@@ -7,21 +7,24 @@ import time
    
 def p03():
     st.subheader("P03 - Atlantico", divider="orange")
-    #m = folium.Map(location=(-16.468104952605543, -54.57546160244493))
-    #st_folium(m)
-    #st.button("PAre")
-    poi = pd.DataFrame({
-    'lat': [-16.468104952605543],
-    'lon': [-54.57546160244493],
-    'name': ['ATLANTICO']
-    })
-    st.map(poi)
+    m = folium.Map(location=[-16.468104952605543, -54.57546160244493], zoom_start=16)
+    folium.Marker(
+    [-16.468104952605543, -54.57546160244493], popup="Vila Goulart", tooltip="Vila Goulart"
+    ).add_to(m)
+    
+    return st_folium(m, width=725, returned_objects=[])
+    #poi = pd.DataFrame({
+    #'lat': [-16.468104952605543],
+    #'lon': [-54.57546160244493],
+    #'name': ['ATLANTICO']
+    #})
+    #st.map(poi)
 def p04():
     st.header("P04 - Vila Goulart", divider="orange")
     st.markdown("Esta é a localização do poço")
-    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+    m = folium.Map(location=[-16.48933413559257, -54.63979329041073], zoom_start=16)
     folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+    [-16.48933413559257, -54.63979329041073], popup="Vila Goulart", tooltip="Vila Goulart"
     ).add_to(m)
     
     return st_folium(m, width=725, returned_objects=[])
