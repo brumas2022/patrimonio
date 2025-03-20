4e47ffe0651c1a481b7bdc08ffc0990c4d52cad3
brumas2022 <marcos.brumatti@gmail.com>
2025-03-20 11:32:34 -0400

    Update fiscais.py

--- fiscais.py
+++ fiscais.py
@@ -3,4 +3,10 @@ import pandas as pd
 
 df = pd.read_excel("RELAÇÃO DE FISCAIS DE CONTRATOS VIGENTES.xlsx", sheet_name=0)
 
-st.dataframe(df)
+options = ["North", "East", "South", "West"]
+selection = st.pills("Directions", options, selection_mode="multi")
+st.markdown(f"Your selected options: {selection}.")
+
+
+if options=="North":
+    st.dataframe(df)
