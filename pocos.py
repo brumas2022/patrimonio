71a304fbc171412d6bf03bc94b6086043b92501f
brumas2022 <marcos.brumatti@gmail.com>
2025-03-23 00:37:17 -0300

    Update pocos.py

--- pocos.py
+++ pocos.py
@@ -9,16 +9,11 @@ def p03():
     st.subheader("P03 - Atlantico", divider="orange")
     m = folium.Map(location=[-16.468104952605543, -54.57546160244493], zoom_start=16)
     folium.Marker(
-    [-16.468104952605543, -54.57546160244493], popup="Vila Goulart", tooltip="Vila Goulart"
+    [-16.468104952605543, -54.57546160244493], popup="Atlantico", tooltip="Atlantico"
     ).add_to(m)
     
     return st_folium(m, width=725, returned_objects=[])
-    #poi = pd.DataFrame({
-    #'lat': [-16.468104952605543],
-    #'lon': [-54.57546160244493],
-    #'name': ['ATLANTICO']
-    #})
-    #st.map(poi)
+    
 def p04():
     st.header("P04 - Vila Goulart", divider="orange")
     st.markdown("Esta é a localização do poço")
