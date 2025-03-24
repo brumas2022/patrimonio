278e8e3737c4032bb4ffca1eae88aec198617fc0
brumas2022 <marcos.brumatti@gmail.com>
2025-03-24 00:06:56 -0300

    Create abertura.py

---
+++ abertura.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config("Pagina Diretoria", layout="wide")
+
+st.markdown("SANEAR - Serviço de Saneamento Ambiental de Rondonópolis")
+with open('style1.css') as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+col=st.columns((1,1))
+col[0].button("CONTRATOS", use_container_width=True)
+col[0].button("FISCAIS", use_container_width=True)
+
+col[1].button("ESTRUTURA SANEAR", use_container_width=True)
+col[1].button("ANDAMENTO DAS OBRAS", use_container_width=True)
