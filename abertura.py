633acdc7059c92043de0f543ebbba53e1a4806d4
brumas2022 <marcos.brumatti@gmail.com>
2025-03-24 08:22:12 -0400

    Update abertura.py

--- abertura.py
+++ abertura.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config("Pagina Diretoria", layout="wide")
+st.set_page_config("Pagina Diretoria")
+st.image("img.png")
 
-st.markdown("SANEAR - Serviço de Saneamento Ambiental de Rondonópolis")
 with open('style1.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 col=st.columns((1,1))
 col[0].button("CONTRATOS", use_container_width=True)
 col[0].button("FISCAIS", use_container_width=True)
+col[0].button("ESTRUTURA SANEAR", use_container_width=True)
 
-col[1].button("ESTRUTURA SANEAR", use_container_width=True)
 col[1].button("ANDAMENTO DAS OBRAS", use_container_width=True)
+col[1].button("CONTROLE ESTOQUE", use_container_width=True)
+
+
