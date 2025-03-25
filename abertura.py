611855582df276ecd7408526a8f1e048ffbb3fbd
brumas2022 <marcos.brumatti@gmail.com>
2025-03-25 14:24:38 -0400

    Update abertura.py

--- abertura.py
+++ abertura.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import webbrowser
 
 st.set_page_config("Pagina Diretoria")
 st.image("img.png")
@@ -8,11 +9,16 @@ with open('style1.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 col=st.columns((1,1))
-col[0].button("CONTRATOS", use_container_width=True)
-col[0].button("FISCAIS", use_container_width=True)
-col[0].button("ESTRUTURA SANEAR", use_container_width=True)
+a = col[0].button("CONTRATOS", use_container_width=True)
+b = col[0].button("FISCAIS", use_container_width=True)
+c = col[0].button("ESTRUTURA SANEAR", use_container_width=True)
 
-col[1].button("ANDAMENTO DAS OBRAS", use_container_width=True)
-col[1].button("CONTROLE ESTOQUE", use_container_width=True)
+d = col[1].button("ANDAMENTO DAS OBRAS", use_container_width=True)
+e = col[1].button("CONTROLE ESTOQUE", use_container_width=True)
+
+if d:
+   webbrowser.open_new_tab("http://obras-contrato.streamlit.app") 
+if e:
+   webbrowser.open_new_tab("http://estoque1.streamlit.app")
 
 
