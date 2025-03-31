1d4fd22621707f9ecc6710d177892e8afa1fc7af
brumas2022 <marcos.brumatti@gmail.com>
2025-03-31 00:25:14 -0300

    Update elevatorias.py

--- elevatorias.py
+++ elevatorias.py
@@ -2,23 +2,27 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
+
+
 def ipanema():
-    st.subheader("EEE IPANEMA", divider="orange")
-    #m = folium.Map(location=(-16.478585175815436, -54.64800486531498))
-    #st_folium(m)
-    #st.button("PAre")
-    poi = pd.DataFrame({
-    'lat': [-16.478585175815436],
-    'lon': [-54.64800486531498],
-    'name': ['IPANEMA']
-    })
-    st.map(poi)
+    st.header("EEE IPANEMA", divider="orange")
+    st.markdown("Esta é a localização da elevatoria")
+    m = folium.Map(location=[-16.478585175815436, -54.64800486531498], zoom_start=16)
+    folium.Marker(
+    [-16.478585175815436, -54.64800486531498], popup="EEE IPANEMA", tooltip="Vila Goulart"
+    ).add_to(m)
     
-def p01():
-    st.subheader("P01 - Atlantico", divider="orange")
-    st.markdown("Esta é a localização do poço")
-def p02():
-    st.header("P02 - Cohab", divider="orange")
-    st.markdown("Esta é a localização do poço")
+    return st_folium(m, width=725, returned_objects=[])
+def canaa():
+    st.header("EEE CANAA", divider="orange")
+    st.markdown("Esta é a localização da elevatoria")
+    m = folium.Map(location=[-16.475795739518535, -54.64324120001045], zoom_start=16)
+    folium.Marker(
+    [-16.475795739518535, -54.64324120001045], popup="EEE CANAA", tooltip="Vila Goulart"
+    ).add_to(m)
+    
+    return st_folium(m, width=725, returned_objects=[])
+    
+
     
     
