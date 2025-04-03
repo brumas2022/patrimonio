d94b8c5ab0e3523b7c139ac86094de6ffdfc6545
brumas2022 <marcos.brumatti@gmail.com>
2025-04-03 00:43:53 -0300

    Update abertura.py

--- abertura.py
+++ abertura.py
@@ -18,7 +18,6 @@ e = col[1].button("CONTROLE ESTOQUE", use_container_width=True)
 f = col[1].button("PLANEJAMENTO ESTRATEGICO", use_container_width=True)
 
 if d:
-   
    webbrowser.open("http://obras-contrato.streamlit.app") 
 if e:
    webbrowser.open_new_tab("http://estoque1.streamlit.app")
