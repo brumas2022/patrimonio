7bce38333bcf17d96ea22dae95881b527ded4192
brumas2022 <marcos.brumatti@gmail.com>
2025-04-04 01:09:34 -0300

    Update atualizar_planilha.py

--- atualizar_planilha.py
+++ atualizar_planilha.py
@@ -5,6 +5,7 @@ def atualizar(nome, idade, sexo):
     workbook = openpyxl.load_workbook("nova.xlsx")
     aba = workbook.active
     aba.append([nome, idade, sexo])
+    workbook.save("nova.xlsx")
     
 nome = input("Nome :")
 idade = input("Idade :")
