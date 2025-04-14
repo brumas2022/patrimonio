fe7c7d941816a241829dff95c464d9a975f7bc23
brumas2022 <marcos.brumatti@gmail.com>
2025-04-13 22:54:11 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -106,7 +106,7 @@ def nad():
    st.dataframe(df_new, hide_index=True, column_config={
         "data envio": st.column_config.DatetimeColumn(
             "data envio",
-            format="D MMM YYYY, h:mm a",
+            format="DD/MM/YYYY",
             
         ),
     })
