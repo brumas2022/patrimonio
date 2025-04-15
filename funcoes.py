090a083c20af4c17d11f2ba27f2c9c4faca4bc78
brumas2022 <marcos.brumatti@gmail.com>
2025-04-14 23:03:41 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-
+import streamlit_pdf_viewer as stf
 
 df = pd.read_excel("DADOS_CONTRATOS.xlsx", sheet_name=0)
 
@@ -10,4 +10,13 @@ lista = [10, 20, 30, 40, 50]
 
 df1=list(map(lambda x: x*10, df['valor']))
 
+botao = st.sidebar.button("Arquivo em pdf")
+
+if botao:
+    arquivo = stf.pdf_viewer("CTR 009-2022 ALPHA CONSTRUTORA EIRELI.pdf")
+
+    st.write(arquivo)
+
+
 print(df1)
+
