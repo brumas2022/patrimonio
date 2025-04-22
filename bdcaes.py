ef47bcf372d3e2d33339032049d7ef077972a1e5
brumas2022 <marcos.brumatti@gmail.com>
2025-04-21 22:23:14 -0300

    Update bdcaes.py

--- bdcaes.py
+++ bdcaes.py
@@ -1,6 +1,6 @@
-#from dotenv import load_dotenv
-import load_dotenv
-import dotenv
+from dotenv import load_dotenv
+#import load_dotenv
+#import dotenv
 import streamlit as st 
 import os
 import requests
