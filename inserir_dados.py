0353bcaa6aebd6410a582781b118d385154462f1
brumas2022 <marcos.brumatti@gmail.com>
2025-04-27 19:24:29 -0300

    Update inserir_dados.py

--- inserir_dados.py
+++ inserir_dados.py
@@ -2,6 +2,19 @@ import streamlit as st
 import pandas as pd
 import openpyxl
 
+def card():
+    st.markdown("<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">", unsafe_allow_html=True)
+    st.markdown("""
+                <div class="card" style="width: 18rem;">
+                     <img src="..." class="card-img-top" alt="...">
+                     <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                     </div>
+                </div>)
+                """, unsafe_allow_html=True)
+
 def atualizar(data, atividades):
     workbook = openpyxl.load_workbook("relatorio.xlsx")
     aba = workbook["Atividades"]
@@ -15,7 +28,7 @@ def nova_medicao(nro_ctr, nro_medicao, data_medicao, valor, nf, data_nf, data_pa
     w_medicao.save("relatorio.xlsx")
 
 
-lista = ["INSERIR DIARIO DE OBRA", "INSERIR MEDICAO", "INSERIR CONTRATO"]
+lista = ["INSERIR DIARIO DE OBRA", "INSERIR MEDICAO", "INSERIR CONTRATO", "CARD"]
 a = st.sidebar.selectbox("Escolha a opção", lista)
 
 if a==lista[0]:
@@ -40,5 +53,8 @@ elif a==lista[1]:
         if st.form_submit_button("Confirmar"):
             nova_medicao(nro_ctr, nro_medicao, data_medicao, valor, nf, data_nf, data_pagto)
     df = pd.read_excel("relatorio.xlsx", sheet_name=1) 
-    st.dataframe(df)  
+    st.dataframe(df) 
+elif a=="CARD":
+    card() 
+ 
         
