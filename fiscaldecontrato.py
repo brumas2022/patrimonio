ea30d6231ca75603fc83a030922a10f83e88f3ba
brumas2022 <marcos.brumatti@gmail.com>
2025-05-16 16:23:56 -0400

    Update fiscaldecontrato.py

--- fiscaldecontrato.py
+++ fiscaldecontrato.py
@@ -8,6 +8,101 @@ from fpdf import FPDF
 
 st.set_page_config(page_title="Contratos do fiscal Marcos Brumatti", layout="wide", menu_items={'About':"Its very cool"})
 
+def imprime_branco():
+     pdf = FPDF("P", "mm", "A4") 
+     pdf.add_page() 
+     pdf.set_font("Arial" , size=10) 
+    #pdf.cell(200, 10, txt = "Bem-vindo ao Python!", ln = 1, align="C")
+    #      pdf.cell(100, 50, txt="Novo texto....avante" )
+    #pdf.cell()"C:/Users/Compras/Documents/GitHub/patrimonio/
+     pdf.image("img.png", x=60, y=10, w=90, h=20) 
+     pdf.text(50, 40, txt="RELATORIO MENSAL DE ACOMPANHAMENTO DE CONTRATO")
+    #pdf.rect(x=10, y=70, w=180, h=8)
+    #pdf.rect(x=10, y=70, w=60, h=8)
+    #
+    # pdf.rect(x=70, y=70, w=60, h=8)
+     pdf.text(11, 50, txt="CONTRATO N° : ")
+     pdf.text(130, 50, txt="DATA DE ABERTURA:")
+     #pdf.text(180, 50, txt=data_inicio)
+     pdf.text(11, 60, txt="CONTRATADO(A)")
+     #pdf.text(51, 60, str(empresa))
+     pdf.rect(10, 55, 40, 10)
+     pdf.rect(50, 55, 150, 10)
+     pdf.text(11, 73, txt="TERMO DO CONTRATO : ")
+            
+            #pdf.text(53, 73, txt=objeto[:65])
+            #pdf.text(53, 78, txt=objeto[65:130])
+            #pdf.text(53, 83, txt=objeto[130:195])
+    
+    
+     pdf.rect(10, 68, 190, 20)
+     pdf.text(11, 95, txt="UNIDADE DETENTORA DO CONTRATO:")
+     pdf.rect(10, 90, 190, 8)
+     pdf.rect(10, 98, 190, 20)
+     pdf.text(11, 105, txt="DATA DO INICIO : ")
+     pdf.text(11, 110, txt="DATA DA CONCLUSAO : ")
+     pdf.text(11, 115, txt="PRAZO DO CONTRATO : ")
+     pdf.text(121, 105, txt="VALOR DO CONTRATO : ")
+     pdf.text(121, 110, txt="LICITACAO : ")
+     pdf.text(121, 115, txt="RECURSO : ")
+     pdf.rect(10, 120, 30, 30)
+     pdf.rect(10, 150, 30, 30)
+     pdf.rect(10, 180, 30, 30)
+     pdf.rect(10, 210, 30, 30)
+     pdf.text(11, 125, txt="Ocorrencias")
+            #pdf.text(51, 125, txt=ocorrecias[:78])  #texto da ocorrencia
+            #pdf.text(51, 130, txt=ocorrecias[78:160])
+            #pdf.text(51, 135, txt=ocorrecias[160:240])    
+     pdf.text(11, 155, txt="Diligencias")
+            #pdf.text(51, 155, txt=diligencia[:78]) # texto da diligencia
+            #pdf.text(51, 160, txt=diligencia[78:160])
+            #pdf.text(51, 165, txt=diligencia[160:240])
+     pdf.text(11, 160, txt="demandas e")
+     pdf.text(11, 165, txt="providências")
+     pdf.text(11, 170, txt="adotadas")
+
+     pdf.text(11, 185, txt="Avaliação dos")
+            #pdf.text(51, 185, txt=avaliacao[:78]) # texto da avaliação
+            #pdf.text(51, 190, txt=avaliacao[78:200])
+            #pdf.text(51, 195, txt=avaliacao[160:240])
+            
+            
+     pdf.text(11, 190, txt="serviços e")
+     pdf.text(11, 195, txt="documentos")
+     pdf.text(11, 200, txt="apresentados")
+     pdf.text(11, 205, txt="pela empresa")
+
+     pdf.text(11, 215, txt="Observacoes /")
+            #pdf.text(51, 215, txt=obs)  # texto da observação
+            #pdf.text(51,215, txt=obs[:78])
+            #pdf.text(51,220, txt=obs[78:180])
+            #pdf.text(51,225, txt=obs[160:240])
+     pdf.text(11, 220, txt="Sugestões /")
+     pdf.text(11, 225, txt="Reclamações")
+
+     pdf.rect(40, 120, 160, 30)
+     pdf.rect(40, 150, 160, 30)
+     pdf.rect(40, 180, 160, 30)
+     pdf.rect(40, 210, 160, 30)
+
+     pdf.rect(10, 260, 100, 8)
+     pdf.text(11, 265, txt="FISCAL DE CONTRATO : ")
+     pdf.rect(10, 268, 100, 8)
+     pdf.text(11, 273, txt=f"PORTARIA N°")
+     pdf.rect(10, 276, 100, 8)
+     pdf.text(11, 281, txt=f"RELATORIO REFERENTE A :")
+
+     pdf.rect(110, 260, 90, 8)
+     pdf.text(130, 265, txt='ASSINATURA')
+     pdf.rect(110, 268, 90, 16)
+
+
+    
+            #pdf.output(f"C:/Users/Compras/Desktop/2025/RELATORIOS/CTR {nro.replace("/", "-")} {empresa}.pdf") 
+            # "C:/Users/Compras/Desktop/2025/RELATORIOS/
+     pdf.output(f'C:/Users/Compras/Desktop/2025/RELATORIOS/CTR Embranco.pdf')    
+        
+
 def inicia_relatorio():
     
               
@@ -157,6 +252,7 @@ with st.form("Relatorio"):
     data_relatorio_1 = data_relatorio.strftime("%d/%m/%Y")
     if st.form_submit_button():
         inicia_relatorio()
+        #imprime_branco()
 
 
 
