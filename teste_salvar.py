0a7fb8624019e2c27af50af7f84f2462f74e97e8
brumas2022 <marcos.brumatti@gmail.com>
2025-05-27 00:04:39 -0300

    Create teste_salvar.py

---
+++ teste_salvar.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+from fpdf import FPDF
+
+
+def imprime():
+    pdf = FPDF("P", "mm", "A4") 
+    pdf.add_page() 
+    pdf.set_font("Arial" , size=10) 
+    #pdf.cell(200, 10, txt = "Bem-vindo ao Python!", ln = 1, align="C")
+    #      pdf.cell(100, 50, txt="Novo texto....avante" )
+    #pdf.cell()"C:/Users/Compras/Documents/GitHub/patrimonio/
+    #pdf.image("img.png", x=60, y=10, w=90, h=20) 
+    pdf.text(50, 40, txt="RELATORIO MENSAL DE ACOMPANHAMENTO DE CONTRATO")
+    pdf.output("meupdf.pdf", dest="")
+     
+    
+    return
+
+imprime()
+
