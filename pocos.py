1d015ff6de86e19b7938c0e5648c1a93f7aece56
brumas2022 <marcos.brumatti@gmail.com>
2025-05-30 15:10:57 -0400

    Update pocos.py

--- pocos.py
+++ pocos.py
@@ -12,7 +12,7 @@ def poco_tubular(nome, lat, long, tipo):
     [lat, long], popup=f"{nome}", tooltip=f"{nome}"
     ).add_to(m)
     
-    return st_folium(m, width=725, returned_objects=[])
+    return # st_folium(m, width=725, returned_objects=[])
     
 df = pd.read_excel("UNIDADES_SANEAR_2025_COORD.xlsx", sheet_name=0)
 #df = pd.read_csv("pocos.csv")
