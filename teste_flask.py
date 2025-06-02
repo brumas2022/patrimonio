3daf42a9b0d00b0492b2f8c0c81a8e05077c5665
brumas2022 <marcos.brumatti@gmail.com>
2025-06-02 16:30:42 -0400

    Update teste_flask.py

--- teste_flask.py
+++ teste_flask.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     return 'PAGINA INICIAL'
+
+@app.route('/beatles')
+def musica():
+    return 'AQUI SO TEREMOS BEATLES'
 
 app.run()
 
