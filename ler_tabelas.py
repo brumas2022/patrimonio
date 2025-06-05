07cf5fd5e7517145ae7564da65a2f465639c04be
brumas2022 <marcos.brumatti@gmail.com>
2025-06-05 17:01:16 -0400

    Update ler_tabelas.py

--- ler_tabelas.py
+++ ler_tabelas.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import tabula
+import streamlit as st 
 
 
-tabela = tabula.read
+tabela = tabula.read_pdf("contratos/arrelatorio_contrato.pdf", pages="all")
+df = tabela[0]
+st.dataframe(df)
