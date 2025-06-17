30911a56498eba13e4e55476a99c43f49e04f59f
brumas2022 <marcos.brumatti@gmail.com>
2025-06-17 10:58:06 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -173,7 +173,7 @@ elif consulta=="POR NOME":
    resultado = df[df['Descricao']==a]
    
    #st.dataframe(resultado, hide_index=True)
-   colnome[1].dataframe(resultado[0:3], hide_index=True)
+   colnome[1].dataframe(resultado[0:1], hide_index=True)
    colnome[0].dataframe(resultado['Item'], hide_index=True, use_container_width=True)
    colnome[0].dataframe(resultado['Descricao'], hide_index=True, use_container_width=True)
    colnome[0].dataframe(resultado['Qtde'], hide_index=True, use_container_width=True)
