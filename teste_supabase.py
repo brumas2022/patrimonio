d373e3c5cc66e3d77c4b2fa600b3f21caa2b38d6
brumas2022 <marcos.brumatti@gmail.com>
2025-07-01 15:17:21 -0400

    Update teste_supabase.py

--- teste_supabase.py
+++ teste_supabase.py
@@ -8,9 +8,9 @@ def caninos():
     link_api='https://ibhcxtnwnonsnycfgjay.supabase.co'
     
     user='ibhcxtnwnonsnycfgjay'
-    key='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliaGN4dG53bm9uc255Y2ZnamF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUxNTM5NjIsImV4cCI6MjAxMDcyOTk2Mn0.PYLOei6RiMbucEqUmTtnmkcjDfIptsiTcNrUCmrBH7c'
-
-    resposta = requests.get(link_api, auth=(user, key))
+    
+    
+    resposta = requests.get(link_api, auth=(user))
 
     #print(resposta)
     #print(resposta.content)
