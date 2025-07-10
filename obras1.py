d399980fdc8ef0b7407a96f4a731e96607ef92a7
brumas2022 <marcos.brumatti@gmail.com>
2025-07-10 14:52:47 -0400

    Update obras1.py

--- obras1.py
+++ obras1.py
@@ -10,6 +10,7 @@ colimage = st.columns((1,1,1))
 
 df_contratos = pd.read_excel("DADOS_CONTRATOS.xlsx")
 df_medicao = pd.read_excel("DADOS_CONTRATOS.xlsx", sheet_name=4)
+df_medicao1  = pd.read_excel("NOVA_MEDICAO.xlsx", sheet_name="medicao")
 
 
 def dados(n):
@@ -32,7 +33,8 @@ def dados(n):
 
 def medicoes(n):
     #n=11
-    df_medicao = pd.read_excel("DADOS_CONTRATOS.xlsx", sheet_name=4)
+    #df_medicao = pd.read_excel("DADOS_CONTRATOS.xlsx", sheet_name=4)
+    df_medicao  = pd.read_excel("NOVA_MEDICAO.xlsx", sheet_name="medicao")
     nro_contrato = f"{df_contratos.iloc[n,1]}"
     df_selecao = df_medicao[df_medicao["CONTRATO"]==nro_contrato]
     
