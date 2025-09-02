18f6241bf3c97988513d0f236727a34369271c8b
brumas2022 <marcos.brumatti@gmail.com>
2025-09-02 15:45:50 -0400

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -140,7 +140,7 @@ data_atualizacao = df.columns[1].strftime("%d/%m/%Y")
 
 st.write("Atualizado em :", data_atualizacao)
 
-df.columns=['Item', 'Descricao', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
+#df.columns=['Item', 'Descricao', 'Unidade', 'Qtde', 'ValorUnit', 'ValorTotal']
 lista_consulta1=["POR ITEM", "POR NOME", "TODOS", "ESTOQUE-ZERO", "NAD", "PREGAO 013", "PREGAO 014", "PREGAO 012"]
 lista_consulta=["POR ITEM", "POR NOME", "TODOS", "ESTOQUE-ZERO"]
 
