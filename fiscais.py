987a41a32435db048fc68b9d0297d77f223495b1
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2025-09-16 15:06:21 -0400

    Update fiscais.py

--- fiscais.py
+++ fiscais.py
@@ -4,6 +4,11 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 df = pd.read_excel("RELAÇÃO DE FISCAIS DE CONTRATOS VIGENTES.xlsx", sheet_name=0)
+df_relacao = df.iloc[3:]
+#df_relacao = df.drop([0, 1]) # apaga linhas do dataframe...detalhe nos colchetes
+df_relacao.columns = ["CONTRATO", "EMPRESA", "TIPO", "FISCAL CONTRATO", "SUPLENTE1", "FISCAL OBRA", "SUPLENTE"]#, "JAN", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO", "JULHO", "AGOSTO"]
+#df_nova = df_relacao.
+
 lista = ["1","2"]
 opcao = df["FISCAL CONTRATO"].tolist()
 st.write(opcao)
@@ -12,11 +17,8 @@ options = ["MARCOS BRUMATTI", "JAMAL BADIE DAUD", "SINVAL RAIMUNDO DA SILVA", \
            "MARIA DA CONCEIÇÃO DE GOIS", "JULIO CESAR SALGADO"]
 selection = st.pills("Fiscais de Contrato", options)
 st.markdown(f"Voce selecionou o seguinte fiscal: {selection}.")
-df_relacao = df.iloc[3:]
-#df_relacao = df.drop([0, 1]) # apaga linhas do dataframe...detalhe nos colchetes
-df_relacao.columns = ["CONTRATO", "EMPRESA", "TIPO", "FISCAL CONTRATO", "SUPLENTE1", "FISCAL OBRA", "SUPLENTE"]#, "JAN", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO", "JULHO", "AGOSTO"]
-#df_nova = df_relacao.
 
+###ESTAVA AQUI
 #st.dataframe(df_relacao)
 
 if selection=="MARCOS BRUMATTI":
