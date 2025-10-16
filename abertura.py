a423cf3445b8fa457eea612cc1ade0b8d734ddc0
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2025-10-16 15:49:37 -0400

    Update abertura.py

--- abertura.py
+++ abertura.py
@@ -8,14 +8,15 @@ st.image("contratos/Logosanear1.jpg")
 with open('style1.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-col=st.columns((1,1))
+col=st.columns((1,1,1))
 a = col[0].link_button("CONTRATOS", "http://obras1.streamlit.app", use_container_width=True)
 b = col[0].link_button("FISCAIS", "https://fiscais.streamlit.app/", use_container_width=True)
 c = col[0].link_button("ESTRUTURA SANEAR", "https://estrutura1.streamlit.app", use_container_width=True)
 
-d = col[1].link_button("ANDAMENTO DAS OBRAS", "http://obras2.streamlit.app", use_container_width=True)
-e = col[1].link_button("CONTROLE ESTOQUE", "http://estoque1.streamlit.app", use_container_width=True)
-f = col[1].link_button("PLANEJAMENTO ESTRATEGICO", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app",  use_container_width=True)
+d = col[2].link_button("ANDAMENTO DAS OBRAS", "http://obras2.streamlit.app", use_container_width=True)
+e = col[2].link_button("CONTROLE ESTOQUE", "http://estoque1.streamlit.app", use_container_width=True)
+f = col[2].link_button("PLANEJAMENTO ESTRATEGICO", "https://comunicasanear-avcnrpqesbpzeg2ecsmuun.streamlit.app",  use_container_width=True)
+
 
 if d:
    webbrowser.open("http://obras-contrato.streamlit.app") 
