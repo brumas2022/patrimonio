920147acdec5f2c1970302b5e8f301208c7c19ed
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2026-01-05 17:30:14 -0400

    Update analise.py

--- analise.py
+++ analise.py
@@ -1,6 +1,31 @@
 import pandas as pd
 import streamlit as st
 
-tabela = pd.read_excel("controle_analise.xlsx", sheet_name=0)
+st.set_page_config("Controle de analise de projetos", layout="wide")
 
-st.dataframe(tabela)
+tabela = pd.read_excel("Controle_de_analise.xlsx", sheet_name=0)
+
+st.dataframe(tabela, hide_index=True, column_config={
+        "data_entrada": st.column_config.DatetimeColumn(
+            "data_entrada",
+            format="DD/MM/YYYY",
+            
+        ),
+        "data_memo": st.column_config.DatetimeColumn(
+            "data_memo",
+            format="DD/MM/YYYY",
+            
+        ),
+        "data_relatorio": st.column_config.DatetimeColumn(
+            "data_relatorio",
+            format="DD/MM/YYYY",
+            
+        ),
+        "data_envio": st.column_config.DatetimeColumn(
+            "data_envio",
+            format="DD/MM/YYYY",
+            
+        ),
+        
+        
+    })
