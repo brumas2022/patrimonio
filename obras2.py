eb0298a6ffef4c1909594dc753775dfa211860e4
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2026-03-30 11:38:48 -0400

    Update obras2.py

--- obras2.py
+++ obras2.py
@@ -15,7 +15,7 @@ df_medicao  = pd.read_excel("NOVA_MEDICAO.xlsx", sheet_name=0)
 
 
 def vencimentos():
-    contratos_vencer = pd.read_excel("marco-2026.xlsx", sheet_name=0)
+    contratos_vencer = pd.read_excel("abril-2026.xlsx", sheet_name=0)
     st.markdown("**CONTRATOS A VENCER EM MARÇO/2026**")
     st.dataframe(contratos_vencer)
     
