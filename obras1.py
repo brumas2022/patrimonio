24956fa10dd49fc6f2af16b2b4c59b4ae17219ee
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2026-05-05 10:37:44 -0400

    Update obras1.py

--- obras1.py
+++ obras1.py
@@ -69,7 +69,7 @@ def main():
            st.write(ctr)
            response1 = supabase.table("bdmedicaonova").select("contrato", "medicao", "datamedicao", "valor", "notafiscal", "datapagto").eq("contrato", ctr).execute()
            #st.dataframe(response1.data)
-           col = st.columns(3, width=2)
+           col = st.columns(3, width=10)
            col[0].markdown("***CONTRATO***" )
            col[1].write("nro da medicao")
            col[2].write("data da medicao")
